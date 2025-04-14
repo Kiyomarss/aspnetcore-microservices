3c1a54e87a91794e2823ac2375c1e4432843cbed
Kiyomarss <kiyomarss.p1997@gmail.com>
2025-04-14 22:12:25 +0330

    Change for run project

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Ordering.Infrastructure.Data;
+
+namespace Ordering.Infrastructure;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+                            .SetBasePath(Directory.GetCurrentDirectory()) // مسیر ریشه پروژه
+                            .AddJsonFile("appsettings.json", optional: false)
+                            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        var connectionString = configuration.GetConnectionString("Database");
+
+        optionsBuilder.UseSqlServer(connectionString); // یا UseNpgsql یا UseSqlite، بسته به دیتابیس
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
